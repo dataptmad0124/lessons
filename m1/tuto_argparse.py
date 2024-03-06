c9175e0b84458fde18b97fe4089bbe9cb76a3d20
iudh <yeho@mail.com>
2024-03-06 18:55:31 +0100

    argparse

--- m1/tuto_argparse.py
+++ m1/tuto_argparse.py
@@ -1,14 +1,33 @@
 import argparse
-
+import pandas as pd
 
 parser = argparse.ArgumentParser(description='BiciMad')
 
-parser.add_argument('-o', '--origin', type=str, help='Origen de usuario')
-parser.add_argument('-d', '--destination', type=str, help='Destino de usuario')
+
+# definicion de argumentos
+parser.add_argument('-o', '--origin', type=str, help='Origen de usuarios')
+parser.add_argument('-d', '--destination', type=str, help='Destino de usuarios')
+
+
+parser.add_argument('-f', '--filter', type=float, help='Filtro para pruebas')
+
+
 
 parser_args = parser.parse_args()
 
+# variables que le paso por terminal, nombre viene del --nombre
 origin = parser_args.origin
 destination = parser_args.destination
 
-print(origin, destination)
+filtro = parser_args.filter
+
+
+
+df = pd.read_csv('data/crop_yield.csv')
+
+
+if filtro > 15:
+    print(df[(df.Fert==origin) & (df.Water==destination) & (df.Yield>filtro)])
+    
+else:
+    print(df[(df.Fert==origin) & (df.Water==destination)])
